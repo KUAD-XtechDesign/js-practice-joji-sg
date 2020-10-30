680252e6feb21d371d7c0e6d8b3aff8f499abc15
joji-sg <68629151+joji-sg@users.noreply.github.com>
2020-10-30 15:50:27 +0900

    add

---
+++ sample/js/script01.js
@@ -0,0 +1,17 @@
+$(function(){
+    $("#button01").on("click",function(){
+        $(".back-red").hide(90);
+    })
+})
+
+$(function(){
+    $("#button02").on("click",function(){
+        $(".back-blue").hide(90);
+    })
+})
+
+$(function(){
+    $("#button03").on("click",function(){
+        $("#box03,#box07").hide(90);
+    })
+})
